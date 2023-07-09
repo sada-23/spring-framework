075d95842e656248530aa5f1b89765d3acedfaca
sada-23 <info-sada-23@gmail.com>
2023-07-09 08:03:29 -0500

    MockitoMVC

---
+++ Spring-23-UnitTesting-MockMvc/src/main/java/com/company/entity/Student.java
@@ -0,0 +1,29 @@
+package com.company.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+}
---
+++ Spring-23-UnitTesting-MockMvc/src/main/java/com/company/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.company.service;
+
+import com.company.entity.Student;
+import com.company.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent() {
+        return studentRepository.findAll();
+    }
+
+}
